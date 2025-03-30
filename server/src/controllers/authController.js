d18202f229451ce1165f6b91553afb2f00f1f555
wongy123 <wongmanshing0226@gmail.com>
2025-03-30 11:37:48 +0000

    Add login logic with JWT

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -4,7 +4,7 @@ const asyncHandler = require('express-async-handler');
 exports.registerUser = asyncHandler(async (req, res, next) => {
     let { userName, displayName = userName, email, password, degree, profileBio = '' } = req.body;
 
-    if (!userName ||  !email || !password || !degree) {
+    if (!userName || !email || !password || !degree) {
         return res.status(400).json({
             success: false,
             message: 'Please provide a username and email',
@@ -30,4 +30,31 @@ exports.registerUser = asyncHandler(async (req, res, next) => {
 });
 
 exports.loginUser = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: 'Please provide an email and password',
+        });
+    }
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user || !(await user.matchPassword(password))) {
+        return res.status(401).json({
+            success: false,
+            message: 'Invalid credentials',
+        });
+    }
+
+    const userObj = user.toObject();
+    delete userObj.password;
+
+    const token = await user.generateJWT();
+    return res.status(200).json({
+        success: true,
+        token,
+        user: userObj,
+    });
 });
--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,6 +1,7 @@
 const expressAsyncHandler = require("express-async-handler");
 const { default: mongoose } = require("mongoose");
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     userName: {
@@ -46,18 +47,18 @@ const userSchema = new mongoose.Schema({
     },
 });
 
-userSchema.pre("save", { document: true, query: false }, asyncHandler(async function (next) {
+userSchema.pre("save", { document: true, query: false }, async function (next) {
     if (!this.isModified("password")) {
         return next();
     }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
-}));
+});
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
-}
+};
 
 userSchema.methods.generateJWT = function () {
     return jwt.sign(
