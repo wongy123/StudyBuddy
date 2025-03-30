1d2e9b4139153e3df43f06e1690ce4ec68e9aeaf
wongy123 <wongmanshing0226@gmail.com>
2025-03-30 13:53:29 +0000

    Add updateUser logic

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/user');
 const asyncHandler = require('express-async-handler');
 
 exports.getAllUsers = asyncHandler(async (req, res, next) => {
-    const users = await User.find().select('-password');
+    const users = await User.find();
     return res.status(200).json({
         success: true,
         count: users.length,
@@ -12,7 +12,7 @@ exports.getAllUsers = asyncHandler(async (req, res, next) => {
 
 exports.getUserById = asyncHandler(async (req, res, next) => {
     const { id } = req.params;
-    const user = await User.findById(id).select('-password');
+    const user = await User.findById(id);
     if (!user) {
         return res.status(404).json({
             success: false,
@@ -26,6 +26,36 @@ exports.getUserById = asyncHandler(async (req, res, next) => {
 });
 
 exports.updateUser = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    if (req.user.id !== id && req.user.role !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: 'You are not authorised to update this user',
+        });
+    }
+
+    const updates = { ...req.body };
+    delete updates.role;
+    delete updates.password;
+    delete updates.email;
+    delete updates.userName;
+
+    const updatedUser = await User.findByIdAndUpdate(id, updates, {
+        new: true,
+        runValidators: true,
+    });
+
+    if (!updatedUser) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found',
+        });
+    }
+
+    res.status(200).json({
+        success: true,
+        data: updatedUser,
+    });
 });
 
 exports.deleteUser = asyncHandler(async (req, res, next) => {
