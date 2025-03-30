93e91af61a79e588b5a446f43193b076c17f0302
wongy123 <wongmanshing0226@gmail.com>
2025-03-30 13:46:56 +0000

    Add authenticateWithJwt middleware

---
+++ server/src/middleware/authenticateWithJwt.js
@@ -0,0 +1,36 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const asyncHandler = require("express-async-handler");
+
+const authenticateWithJwt = asyncHandler(async (req, res, next) => {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+        return res.status(401).json({
+            success: false,
+            message: "No token provided, authorisation denied",
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const user = await User.findById(decoded.id);
+
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Token is not valid",
+        });
+    }
+});
+
+module.exports = authenticateWithJwt;
