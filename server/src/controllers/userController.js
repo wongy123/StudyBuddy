669aca28c021719ce1435eceaffcaf7baa297571
Ubuntu <ubuntu@ip-172-31-100-128.ap-southeast-2.compute.internal>
2025-03-30 02:27:32 +0000

    Add getUserById

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -11,6 +11,18 @@ exports.getAllUsers = asyncHandler(async (req, res, next) => {
 });
 
 exports.getUserById = asyncHandler(async (req, res, next) => {
+    const { id } = req.params;
+    const user = await User.findById(id).select('-password');
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: 'User not found',
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: user,
+    });
 });
 
 exports.createUser = asyncHandler(async (req, res, next) => {
