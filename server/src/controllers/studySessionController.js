b439726b7f1260175dc0a52a689a975261a36b1b
wongy123 <wongmanshing0226@gmail.com>
2025-04-01 02:34:50 +0000

    Modify studySession controller to use modmin middleware

--- server/src/controllers/studySessionController.js
+++ server/src/controllers/studySessionController.js
@@ -14,10 +14,11 @@ exports.getAllSessions = asyncHandler(async (req, res, next) => {
 
 exports.getSessionById = asyncHandler(async (req, res, next) => {
     const session = await StudySession.findById(req.params.id);
-
     if (!session) {
-        res.status(404);
-        throw new Error('Study session not found');
+        return res.status(404).json({
+            success: false,
+            message: `Study session not found`,
+        });
     }
 
     res.status(200).json(session);
@@ -44,23 +45,7 @@ exports.createSession = asyncHandler(async (req, res, next) => {
 });
 
 exports.updateSession = asyncHandler(async (req, res, next) => {
-    const session = await StudySession.findById(req.params.id);
-
-    if (!session) {
-        return res.status(404).json({
-            success: false,
-            message: "Study session not found",
-        });
-    }
-
-    const isCreator = session.createdBy.equals(req.user._id);
-
-    if (!isCreator && !isModmin(req.user)) {
-        return res.status(403).json({
-            success: false,
-            message: "You are not authorised to update this session",
-        });
-    }
+    const session = req.resource;
 
     const allowedFields = [
         "title",
@@ -87,4 +72,9 @@ exports.updateSession = asyncHandler(async (req, res, next) => {
 });
 
 exports.deleteSession = asyncHandler(async (req, res, next) => {
+    const session = req.resource;
+
+    await session.deleteOne();
+
+    res.status(204).send();
 });
