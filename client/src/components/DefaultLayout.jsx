f727235e62f2b1c9b50954c6032826be93a3cc5d
wongy123 <wongmanshing0226@gmail.com>
2025-04-18 06:11:20 +0000

    Hide sidebar if screensize smaller than lg

--- client/src/components/DefaultLayout.jsx
+++ client/src/components/DefaultLayout.jsx
@@ -1,29 +1,29 @@
 // src/components/DefaultLayout.jsx
-import { Outlet } from 'react-router-dom';
-import {
-    AppBar,
-    Box,
-    Drawer,
-    Toolbar,
-    Typography,
-} from '@mui/material';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import { Outlet } from "react-router-dom";
+import { Box, Toolbar, useMediaQuery, useTheme } from "@mui/material";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const DefaultLayout = () => {
-    return (
-        <Box sx={{ display: 'flex', height: '100vh'}}>
-            <Header />
+  const theme = useTheme();
+  const lgDown = useMediaQuery(theme.breakpoints.down("lg"));
 
-            <Sidebar />
+  return (
+    <Box sx={{ display: "flex", height: "100vh" }}>
+      <Header />
 
-            {/* Main Content on the right */}
-            <Box component="main" sx={{ flexGrow: 1, p: 3, overflowY: 'auto', height: '100vh', }}>
-                <Toolbar /> {/* Spacer to align with AppBar */}
-                <Outlet />
-            </Box>
-        </Box>
-    );
+      {!lgDown && <Sidebar />}
+
+      {/* Main Content on the right */}
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, p: 3, overflowY: "auto", height: "100vh" }}
+      >
+        <Toolbar /> {/* Spacer to align with AppBar */}
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default DefaultLayout;
