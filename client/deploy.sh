d7f1cda43ae398929a627f490c4fc4352ae08cdd
Wongy123 <wongmanshing0226@gmail.com>
2025-07-31 00:38:12 +1000

    Change deploy script to be fully automated

--- client/deploy.sh
+++ client/deploy.sh
@@ -5,6 +5,11 @@ APP_NAME="StudyBuddy"
 DEPLOY_PATH="/var/www/html/$APP_NAME"
 BUILD_PATH="dist"
 
+sudo mkdir -p "$DEPLOY_PATH"
+
 npm run build
 sudo rm -rf "$DEPLOY_PATH"/*
 sudo cp -r "$BUILD_PATH"/* "$DEPLOY_PATH"/
+
+sudo chown caddy:caddy -R "$DEPLOY_PATH"
+sudo chown -R 755 "$DEPLOY_PATH"
